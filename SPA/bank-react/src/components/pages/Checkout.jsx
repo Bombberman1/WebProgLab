d66d99c9c83e923aeaad1acb270a30ded3718f9d
Oleg <lezik569@gmail.com>
2023-12-11 22:47:38 +0200

    Full fixed NovaPoshta

--- SPA/bank-react/src/components/pages/Checkout.jsx
+++ SPA/bank-react/src/components/pages/Checkout.jsx
@@ -1,10 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import { useAsyncValue, useNavigate } from "react-router-dom";
 import { BackButton, CheckoutButtonsContainer, CheckoutHeader, 
     CheckoutPageContainer, ContinueButton, 
     FieldContainer, FieldText, FieldsContainer } from "./styled/Checkout.styled";
 import { Formik, Field, Form } from "formik";
 import { validation } from "./checkout-components/validation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ErrorMessage from "./checkout-components/ErrorMessage";
 import NovaPoshtaInfo from "./checkout-components/NovaPoshtaInfo";
 
@@ -13,13 +13,13 @@ function CheckoutPage() {
     const navigate = useNavigate();
     const [errorsText, setErrorsText] = useState([]);
     const [novaOffice, setNovaOffice] = useState(undefined);
-    const [successClick, setSuccessClick] = useState(0);
+    const prevErrorsTextRef = useRef(errorsText);
 
     const apiKey = '3fed9303d9bac12288640094b5185684';
     const apiUrl = 'https://api.novaposhta.ua/v2.0/json/';
 
     const getOfficesInTown = async (city, number) => {
-        if (/^[A-z]+$/.test(city)) {
+        if (/[A-z]+/.test(city)) {
             setErrorsText((errors) => [...errors, "Town should be UA language"]);
             return;
         }
@@ -42,7 +42,7 @@ function CheckoutPage() {
         const data = await response.json();
 
         if (data.success && data.data.length > 0) {
-            const exactOffice = data.data.find((office) => (new RegExp(`^(Відділення|Поштомат "Нова Пошта") №${number}[:\\s]`)).test(office.Description));
+            const exactOffice = data.data.find((office) => (new RegExp(`(Відділення|Пункт|Поштомат "Нова Пошта") №${number}[:\\s]`)).test(office.Description));
             if (exactOffice) {
                 return exactOffice;
             } else {
@@ -55,34 +55,38 @@ function CheckoutPage() {
         }
     };
 
+    useEffect(() => {
+        if (prevErrorsTextRef.current !== errorsText) {
+            if (!errorsText.length) {
+                alert('Wait 10 seconds');
+                const timeoutId = setTimeout(() => {
+                    navigate('/success');
+                }, 10000);
+    
+                return () => clearTimeout(timeoutId);
+            }
+        }
+    
+        prevErrorsTextRef.current = errorsText;
+    }, [errorsText, navigate]);
   
     return (
         <CheckoutPageContainer>
             <CheckoutHeader>Checkout</CheckoutHeader>
             <Formik
                 initialValues={{ firstName: "", lastName: "", email: "", phone: "", age: "", address: "" }}
-                onSubmit={async (values) => {
+                onSubmit={(values) => {
                     const errors = validation(values);
-                    setErrorsText(() => [...errors]);
+                    setErrorsText([...errors]);
 
                     const params = values.address.match(/([^\s,.:_]+|\S\d+)/g);
                     if (params.length === 2) {
-                        const res = await getOfficesInTown(params[0], params[1]);
-                        setNovaOffice(res);
-                        if (res === undefined) {
-                            setSuccessClick(0);
-                        }
+                        getOfficesInTown(params[0], params[1]).then((res) => {
+                            setNovaOffice(res);
+                        });
                     } else {
-                        setErrorsText((err) => [...err, "Invalid format, example: Київ, 5"]);
-                        console.log(errorsText);
-                        setSuccessClick(0);
-                    }
-
-                    if (!errors.length) {
-                        setSuccessClick((value) => value + 1);
-                        if (successClick === 1) {
-                            navigate('/success');
-                        }
+                        setErrorsText([...errors, "Invalid format, example: Київ, 5"]);
+                        setNovaOffice(undefined);
                     }
                 }}
             >
